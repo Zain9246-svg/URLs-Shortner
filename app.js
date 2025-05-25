627c64711e1a4ffa02e1318ca94e2f44cb411514
Zain ul hassan <zainulhassan031217@gmail.com>
2025-05-25 08:31:45 +0500

    update

--- app.js
+++ app.js
@@ -90,6 +90,6 @@ const server = createServer(async (req, res) => {
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`Data file path: ${DATA_FILE}`);
+    
     
 });
